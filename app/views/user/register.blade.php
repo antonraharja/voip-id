29d47160b6f5fae877baa671fa9e5cb387f4d843
Anton Raharja <antonrd@gmail.com>
2014-03-25 09:38:51 +0700

    add ide helper https://github.com/barryvdh/laravel-ide-helper

--- app/views/user/register.blade.php
+++ app/views/user/register.blade.php
@@ -5,7 +5,40 @@
 @stop
 
 @section('content')
-	<div class="container">
-		<h1>Register</h1>
+<div class="container">
+	<h1>Register</h1>
+
+	<!-- Success-Messages -->
+	@if ($success = Session::get('success'))
+	<div class="alert alert-success alert-block">
+		<button type="button" class="close" data-dismiss="alert">&times;</button>
+		{{{ $success }}}
 	</div>
+	@endif
+
+	<!-- Error-Messages -->
+	@if ($error = Session::get('error'))
+	<div class="alert alert-danger alert-block">
+		<button type="button" class="close" data-dismiss="alert">&times;</button>
+		{{{ $error }}}
+	</div>
+	@endif
+
+	{{ Form::open(array('url' => 'user/register', 'method' => 'post')) }}
+
+	<div class="form-group">
+		{{ Form::label('username', 'Username') }}
+		{{ Form::text('username') }}
+	</div>
+
+	<div class="form-group">
+		{{ Form::label('password', 'Password') }}
+		{{ Form::password('password') }}
+	</div>
+
+	{{ Form::submit('Login', array('class' => 'btn btn-primary')) }}
+
+	{{ Form::close() }}
+
+</div>
 @stop
