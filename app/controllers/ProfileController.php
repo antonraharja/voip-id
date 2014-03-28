cf5045be0ce8b734ed4721a8c6591eef18767683
Anton Raharja <antonrd@gmail.com>
2014-03-28 14:43:40 +0700

    update ProfileController, code cleanups

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -1,66 +1,66 @@
 <?php
 
 class ProfileController extends BaseController {
-    
-    /**
-     * Instantiate a new DashboardController instance.
-     */
-    public function __construct() {
 
-        $this->beforeFilter('auth');
+	/**
+	 * Instantiate a new DashboardController instance.
+	 */
+	public function __construct() {
 
-    }
-    
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
-    public function index() {
+		$this->beforeFilter('auth');
 
-    	$profile = Profile::find(Auth::user()->profile_id);
+	}
+	
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index() {
 
-        return View::make('profile.index')->with('profile', $profile);
+		$profile = Profile::find(Auth::user()->profile_id);
 
-    }
-    
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function update($id) {
+		return View::make('profile.index')->with('profile', $profile);
 
-	$input = Input::only('first_name', 'last_name', 'website');
+	}
+	
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id) {
 
-	$rules = array(
-		'first_name' => 'required',
-	);
-	$v = Validator::make($input, $rules);
-	if ($v->fails()) {
-		return Output::push(array('path' => 'profile', 'errors' => $v, 'input' => TRUE));
+		$input = Input::only('first_name', 'last_name', 'website');
+
+		$rules = array(
+			'first_name' => 'required',
+		);
+		$v = Validator::make($input, $rules);
+		if ($v->fails()) {
+			return Output::push(array('path' => 'profile', 'errors' => $v, 'input' => TRUE));
+		}
+
+		if ($id && Auth::user()->profile_id == $id) {
+			$profile = Profile::find($id);
+			$profile->first_name = $input['first_name']; 
+			$profile->last_name = $input['last_name']; 
+			$profile->website = $input['website']; 
+			$profile->save();
+
+			return Output::push(array(
+				'path' => 'profile',
+				'messages' => array('success', _('Profile has been saved')),
+				));
+		} else {
+			return Output::push(array(
+				'path' => 'profile',
+				'messages' => array('fail', _('Unable to update profile')),
+				'input' => TRUE
+				));
+		}
+
 	}
 
-    	if ($id && Auth::user()->profile_id == $id) {
-    		$profile = Profile::find($id);
-		$profile->first_name = $input['first_name']; 
-		$profile->last_name = $input['last_name']; 
-		$profile->website = $input['website']; 
-		$profile->save();
-
-    		return Output::push(array(
-    			'path' => 'profile',
-    			'messages' => array('success', _('Profile has been saved')),
-    			));
-    	} else {
-    		return Output::push(array(
-    			'path' => 'profile',
-    			'messages' => array('fail', _('Unable to update profile')),
-    			'input' => TRUE
-    			));
-    	}
-
-    }
-    
 }
