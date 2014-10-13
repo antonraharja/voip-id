1ec62448ad2bb6814e4619a7c41a9faecaaa8f1f
rahman <rahman@mediainfo.co.id>
2014-10-14 06:37:46 +0700

    check if table setting exist

--- app/config/settings.php
+++ app/config/settings.php
@@ -11,9 +11,10 @@ $list = array();
 $format = function(&$list, $keys, $val) use(&$format) {
     $keys ? $format($list[array_shift($keys)], $keys, $val) : $list = $val;
 };
-
-foreach(Setting::all() as $setting) {
-    $format($list, explode('.', $setting->name), $setting->value);
+if(Schema::hasTable('settings')) {
+    foreach (Setting::all() as $setting) {
+        $format($list, explode('.', $setting->name), $setting->value);
+    }
 }
 
 return $list;
