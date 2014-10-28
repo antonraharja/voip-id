b401199feed069cec100e334639b1eb1a742e9b1
rahman <rahman@mediainfo.co.id>
2014-10-28 13:55:06 +0700

    add development message

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -8,6 +8,12 @@
 	<div class="container">
 		<h1>{{ _('Home') }}</h1>
 		@include('template.messages')
+
+		<div class="well">
+		    We are currently in development stage.<br>
+		    Data might be reset at anytime.<br>
+		    Thank you for participating with us.
+		</div>
 	</div>	
 @stop
 
