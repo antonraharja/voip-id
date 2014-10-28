ad4280c6589eabcacce059676a0fbbbea47584a4
rahman <rahman@mediainfo.co.id>
2014-10-28 13:39:31 +0700

    move cdn to local

--- app/views/template/skeleton.blade.php
+++ app/views/template/skeleton.blade.php
@@ -7,8 +7,8 @@
 			@show
 		</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/cosmo/bootstrap.min.css" rel="stylesheet">
-		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		{{ HTML::style('css/bootstrap.min.css') }}
+		{{ HTML::style('css/font-awesome.min.css') }}
 		{{ HTML::style('css/style.css') }}
 	</head>
 
@@ -20,8 +20,8 @@
 		@include('template.footer')
 
 		<!-- Scripts -->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		{{ HTML::script('js/jquery.min.js') }}
+		{{ HTML::script('js/bootstrap.min.js') }}
 		{{ HTML::script('js/plugins.js') }}
 		{{ HTML::script('js/script.js') }}
 	</body>
