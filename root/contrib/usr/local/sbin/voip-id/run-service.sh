860de9e139ef91a12aaec8531edb6e8b033e6f4b
Asoka Wardhana <asoka@itmn.co.id>
2014-11-03 21:51:06 +0700

    update script sync_domain, add script run-service.sh

---
+++ root/contrib/usr/local/sbin/voip-id/run-service.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DATE=$(date +"%Y-%m-%d %T")
+STATUS=`ls /var/run/opensips/opensips.pid |wc -l |tr -d 't\n\r'`
+
+if [ "$STATUS" == "1" ];then
+	CHECKPS=`cat /usr/local/sbin/voip-id/run-status |tr -d 't\n\r'`
+	if [ "$CHECKPS" == "1" ]; then
+		echo "[ERROR] [$DATE] INFO:CANNNOT SYNC, DAEMON STILL RUNNING" >> /var/log/opensips/sync.log
+	else
+		echo 1 > /usr/local/sbin/voip-id/run-status
+		/usr/bin/php /usr/local/sbin/voip-id/sync_daemon.php >/dev/null 2>&1
+		sleep 2
+		echo 0 > /usr/local/sbin/voip-id/run-status
+	fi
+else
+	echo "[ERROR] [$DATE] INFO:CANNNOT SYNC, OPENSIPS NOT RUNNING" >> /var/log/opensips/sync.log
+fi
+
+exit 0
--- root/contrib/usr/local/sbin/voip-id/sync_domain.php
+++ root/contrib/usr/local/sbin/voip-id/sync_domain.php
@@ -6,7 +6,7 @@ if(!$conn){
     die('Could not connect: ' . mysql_error());
 }
 
-$r = mysql_query('SELECT domain from voip_id.domains');
+$r = mysql_query('SELECT domain from voip_id.domains WHERE deleted_at IS NULL');
 if(mysql_num_rows($r) == 0){
 	$domain1 = NULL;
 }else{
@@ -28,10 +28,21 @@ if(mysql_num_rows($r) == 0){
 	}
 }
 
+$r = mysql_query('SELECT domain from voip_id.domains WHERE deleted_at IS NOT NULL');
+if(mysql_num_rows($r) == 0){
+	$domain3 = NULL;
+}else{
+	while($row = mysql_fetch_row($r)){
+		foreach($row as $key => $value){
+		$domain3[] = $value;
+		}
+	}
+}
+
 if(!empty($domain1)){
 	if(!empty($domain2)){
-		$diff1 = array_diff($domain1, $domain2);
-		foreach($diff1 as $key => $value){
+		$diff = array_diff($domain1, $domain2);
+		foreach($diff as $key => $value){
 			$cmd = "/usr/sbin/opensipsctl domain add $value";
 			exec($cmd);
 		}
@@ -44,16 +55,12 @@ if(!empty($domain1)){
 }
 
 if(!empty($domain2)){
-	if(!empty($domain1)){
-		$diff2 = array_diff($domain2, $domain1);
-		foreach($diff2 as $key => $value){
+	if(!empty($domain3)){
+		$intersect = array_intersect($domain3, $domain2);
+		foreach($intersect as $key => $value){
 			$cmd = "/usr/sbin/opensipsctl domain rm $value";
 			exec($cmd);
 		}
-	}else{
-		foreach($domain2 as $key => $value)
-			$cmd = "/usr/sbin/opensipsctl domain rm $value";
-			exec($cmd);
 	}
 }
 
