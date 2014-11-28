486e4868d73f238f7187e876ab05692dfdc59935
Rahman <rahman@mediainfo.co.id>
2014-11-28 16:47:39 +0700

    add event for notification

--- app/events.php
+++ app/events.php
@@ -20,6 +20,10 @@ Event::listen('auth.logout', function($user)
 
 Event::listen('logger', function($log)
 {
+    if(Config::get('settings.email_address_for_admin')){
+        Event::fire('notification', array($log));
+    }
+
     $log = array(
         'event_name'=> $log[0],
         'custom_parameter'=> json_encode($log[1]),
@@ -35,4 +39,27 @@ Event::listen('logger', function($log)
 
     File::append(Config::get('settings.log_file'),$log_string);
 
+});
+
+Event::listen('notification', function($log)
+{
+    $allowed_event = array('domain_add','domain_remove','account_add','account_remove','phone_number_add','phone_number_remove');
+    if(in_array($log[0], $allowed_event)) {
+        $log = array(
+            'event_name' => $log[0],
+            'custom_parameter' => $log[1],
+            'verbose_level' => $log[2],
+            'ip_address' => Request::getClientIp(),
+            'request_uri' => Request::url(),
+        );
+
+        Mail::send('emails.notification', array('data' => $log), function ($message) {
+            $message->from(
+                Config::get('startup.email_sender.address'),
+                Config::get('startup.email_sender.name')
+            )
+                ->to(Config::get('settings.email_address_for_admin'))
+                ->subject(_('Notification'));
+        });
+    }
 });
