823e9b73fe2ec98e5b2e0d134ea7198b6676fac4
Anton Raharja <antonrd@gmail.com>
2014-11-29 12:09:07 +0700

    update home

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -10,9 +10,18 @@
 		@include('template.messages')
 
 		<div class="well">
-		    We are currently in development stage.<br>
-		    Data might be reset at anytime.<br>
-		    Thank you for participating with us.
+			<p>Terima kasih telah mengunjungi website kami.</p>
+			
+			<p>Saat ini kami masih dalam persiapan rilis.<br />
+			Pengembangan dan perbaikan masih terus dilakukan. Anda dapat mengikuti perkembangannya <a href="http://bit.ly/teleponrakyat" target="_blank">disini</a></p>
+			
+			<p>Kendati demikian, layanan pada website ini sudah berjalan dengan baik. Apabila ada kekurangan atau kesalahan biasanya tidak lama kemudian langsung diperbaiki</p>
+			
+			<p>Oleh karena daftarkanlah diri anda sebagai Manager dan tambahkan Domain anda disini untuk mulai</p>
+			
+			<p>Baca petunjuk untuk memulai <a href="http://bit.ly/memulaiteleponrakyat" target="_blank">disini</a>.</p>
+			
+			<p>Apabila ditemui kesulitan atau ingin berdiskusi dengan kami maka sila hubungi kami dengan bergabung di group facebook kami <a href="https://www.facebook.com/groups/voipid/" target="_blank">disini</a>.</p>
 		</div>
 	</div>	
 @stop
