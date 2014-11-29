65baa2044bceda5ae953505f49a78618088e9274
Anton Raharja <antonrd@gmail.com>
2014-11-30 03:55:53 +0700

    update home to use s.id

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -12,16 +12,15 @@
 		<div class="well">
 			<p>Terima kasih telah mengunjungi website kami.</p>
 			
-			<p>Saat ini kami masih dalam persiapan rilis. Pengembangan dan perbaikan masih terus dilakukan. Anda dapat mengikuti perkembangannya <a href="http://bit.ly/teleponrakyat" target="_blank">disini</a>.</p>
+			<p>Saat ini kami masih dalam persiapan rilis. Pengembangan dan perbaikan masih terus dilakukan. Anda dapat mengikuti perkembangannya <a href="http://s.id/bagitugasteleponrakyat" target="_blank">disini</a>.</p>
 			
 			<p>Kendati demikian, layanan pada website ini sudah berjalan dengan cukup baik. Apabila ada kekurangan atau kesalahan biasanya tidak lama kemudian langsung diperbaiki.</p>
 			
 			<p>Oleh karena ini besar harapan kami agar anda dapat ikut berpartisipasi. Daftarkanlah diri anda sebagai Manager dan tambahkan Domain anda.</p>
 			
-			<p>Baca petunjuk untuk memulai <a href="http://bit.ly/memulaiteleponrakyat" target="_blank">disini</a>.</p>
+			<p>Baca petunjuk untuk memulai <a href="http://s.id/memulaiteleponrakyat" target="_blank">disini</a>.</p>
 			
 			<p>Apabila ditemui kesulitan atau ingin berdiskusi dengan kami maka sila hubungi kami dengan bergabung di Facebook Group kami <a href="https://www.facebook.com/groups/voipid/" target="_blank">disini</a>.</p>
 		</div>
 	</div>	
 @stop
-
