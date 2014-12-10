d31cd5ec80bcd552569084b10b494edc0cf84273
rahman <rahman@mediainfo.co.id>
2014-12-10 10:44:25 +0700

    update dashboard

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -9,7 +9,9 @@
 		<h1>{{ _('Dashboard') }}</h1>
 		<p>{{ _('Welcome') }} <strong>{{ ucwords(Auth::user()->profile->first_name) }} {{ ucwords(Auth::user()->profile->last_name) }}</strong><p>
 		<p>{{ _('Logged in user') }} <strong>{{ Auth::user()->username }}</strong></p>
-		{{ $homepage }}
+		@if(Cookie::get('domain_hash'))
+        {{ $homepage }}
+        @else
 	</div>
 
 @stop
