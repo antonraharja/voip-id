eb3cdf7286f5b0ba8240e444471983450700f634
rahman <rahman@mediainfo.co.id>
2014-12-12 14:15:56 +0700

    Fix validitaion username

--- app/validators.php
+++ app/validators.php
@@ -9,7 +9,7 @@
 Validator::extend('must_alpha_num', function($attribute, $value, $parameters)
 {
     $ret = FALSE;
-    if (preg_match("/^[a-z][a-z0-9]$/", $value)) {
+    if (preg_match("/^[a-z][a-z0-9]{3,31}$/", $value)) {
         $ret = TRUE;
     }
     return $ret;
