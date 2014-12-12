1b2af1a9c17b0dc64277d390cc7922784c25806f
Asoka Wardhana <asoka@itmn.co.id>
2014-12-12 11:16:50 +0700

    update script sync_domain.php & sync_user.php

--- contrib/scripts/usr/local/sbin/voip-id/run-service.sh
+++ contrib/scripts/usr/local/sbin/voip-id/run-service.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 DATE=$(date +"%Y-%m-%d %T")
-STATUS=`ls /var/run/opensips/opensips.pid |wc -l |tr -d 't\n\r'`
+STATUS=`ls /var/run/opensips/opensips.pid |wc -l |tr -d '\n'`
 
 if [ "$STATUS" == "1" ];then
-	CHECKPS=`cat /usr/local/sbin/voip-id/run-status |tr -d 't\n\r'`
+	CHECKPS=`cat /usr/local/sbin/teleponrakyat/run-status |tr -d '\n'`
 	if [ "$CHECKPS" == "1" ]; then
 		echo "[ERROR] [$DATE] INFO:CANNNOT SYNC, DAEMON STILL RUNNING" >> /var/log/opensips/sync.log
 	else
-		echo 1 > /usr/local/sbin/voip-id/run-status
-		/usr/bin/php /usr/local/sbin/voip-id/sync_domain.php >/dev/null 2>&1
+		echo 1 > /usr/local/sbin/teleponrakyat/run-status
+		/usr/bin/php /usr/local/sbin/teleponrakyat/sync_user.php >/dev/null 2>&1
+		sleep 3
+		/usr/bin/php /usr/local/sbin/teleponrakyat/sync_domain.php >/dev/null 2>&1
 		sleep 2
-		/usr/bin/php /usr/local/sbin/voip-id/sync_user.php >/dev/null 2>&1
-		sleep 2
-		echo 0 > /usr/local/sbin/voip-id/run-status
+		echo 0 > /usr/local/sbin/teleponrakyat/run-status
 	fi
 else
 	echo "[ERROR] [$DATE] INFO:CANNNOT SYNC, OPENSIPS NOT RUNNING" >> /var/log/opensips/sync.log
