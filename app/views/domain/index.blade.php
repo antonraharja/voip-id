97ba87caad15c286aad2289a3bdf161ce8a3c830
rahman <rahman@mediainfo.co.id>
2014-12-15 14:59:05 +0700

    update tooltip edit domain

--- app/views/domain/index.blade.php
+++ app/views/domain/index.blade.php
@@ -59,7 +59,7 @@
                     <td>{{ $domain->description }}</td>
                     <td>{{ Form::text('description', url(Config::get('settings.panel_path')).'/'.$domain->id, array('class' => 'form-control input-sm', 'readonly')) }}</td>
                     <td class="text-center action">
-                        <a class="tooltips" href="{{ url('domain/edit/'.$domain->id) }}" title="{{ _('Edit account') }}"><span class="glyphicon glyphicon-pencil"></span></a>
+                        <a class="tooltips" href="{{ url('domain/edit/'.$domain->id) }}" title="{{ _('Edit domain') }}"><span class="glyphicon glyphicon-pencil"></span></a>
                         <a class="tooltips" href="{{ url('domain/users/'.$domain->id) }}" title="{{ _('Manage users') }}"><span class="glyphicon glyphicon-user"></span></a>
                         <a class="tooltips" href="{{ url('phone_number/manage/'.$domain->id) }}" title="{{ _('Manage phone number') }}"><span class="glyphicon glyphicon-phone-alt"></span></a>
                         <a class="tooltips" href="{{ url('domain/delete/'.$domain->id) }}" title="{{ _('Delete domain') }}"><span class="glyphicon glyphicon-trash" onclick="return confirm('{{ _('Are you sure want to delete?') }}')"></span></a>
