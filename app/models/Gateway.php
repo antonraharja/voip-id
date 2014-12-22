f04e9bbec03b9a7fb20627c310ab1530dc2dcf42
rahman <rahman@mediainfo.co.id>
2014-12-23 03:06:14 +0700

    generate docblock

--- app/models/Gateway.php
+++ app/models/Gateway.php
@@ -4,6 +4,22 @@
  * Domain
  *
  * @property-read \User $user
+ * @property integer $id
+ * @property integer $user_id
+ * @property string $gateway_name
+ * @property string $gateway_address
+ * @property integer $prefix
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property \Carbon\Carbon $deleted_at
+ * @method static \Illuminate\Database\Query\Builder|\Gateway whereId($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Gateway whereUserId($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Gateway whereGatewayName($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Gateway whereGatewayAddress($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Gateway wherePrefix($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Gateway whereCreatedAt($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Gateway whereUpdatedAt($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Gateway whereDeletedAt($value) 
  */
 class Gateway extends Eloquent {
 
