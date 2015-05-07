d6fdcbd70f43458a8272bad6d7fc4123f99cb54c
Anton Raharja <antonrd@gmail.com>
2015-05-07 19:34:21 +0700

    remove ide helper

--- app/config/app.php
+++ app/config/app.php
@@ -106,7 +106,6 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-		'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
 		'Aamant\EmailConfirmation\EmailConfirmationServiceProvider',
 		//'Bllim\Datatables\DatatablesServiceProvider',
 	),
