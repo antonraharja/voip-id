9545eafb8034269958d164ed22ec2eaa42a66622
Anton Raharja <antonrd@gmail.com>
2015-05-08 09:21:03 +0700

    rename teleponrakyat to voip-id on contrib script

--- contrib/scripts/usr/local/sbin/voip-id/run-service-cleaner.sh
+++ contrib/scripts/usr/local/sbin/voip-id/run-service-cleaner.sh
@@ -4,16 +4,16 @@ DATE=$(date +"%Y-%m-%d %T")
 STATUS=`ls /var/run/opensips/opensips.pid |wc -l |tr -d '\n'`
 
 if [ "$STATUS" == "1" ];then
-	CHECKPS=`cat /usr/local/sbin/teleponrakyat/run-status-cleaner |tr -d '\n'`
+	CHECKPS=`cat /usr/local/sbin/voip-id/run-status-cleaner |tr -d '\n'`
 	if [ "$CHECKPS" == "1" ]; then
 		echo "[ERROR] [$DATE] INFO:CANNNOT CLEAN, DAEMON STILL RUNNING" >> /var/log/opensips/cleaner.log
 	else
-		echo 1 > /usr/local/sbin/teleponrakyat/run-status-cleaner
-		/usr/bin/php /usr/local/sbin/teleponrakyat/cleaner_user.php >/dev/null 2>&1
+		echo 1 > /usr/local/sbin/voip-id/run-status-cleaner
+		/usr/bin/php /usr/local/sbin/voip-id/cleaner_user.php >/dev/null 2>&1
 		sleep 3
-		/usr/bin/php /usr/local/sbin/teleponrakyat/cleaner_domain.php >/dev/null 2>&1
+		/usr/bin/php /usr/local/sbin/voip-id/cleaner_domain.php >/dev/null 2>&1
 		sleep 2
-		echo 0 > /usr/local/sbin/teleponrakyat/run-status-cleaner
+		echo 0 > /usr/local/sbin/voip-id/run-status-cleaner
 	fi
 else
 	echo "[ERROR] [$DATE] INFO:CANNNOT CLEAN, OPENSIPS NOT RUNNING" >> /var/log/opensips/cleaner.log
