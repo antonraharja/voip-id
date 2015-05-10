38d91973efcca2b097eeb61829bd676b6f14ce96
Anton Raharja <antonrd@gmail.com>
2015-05-10 12:33:36 +0700

    typo home.blade.php

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -14,7 +14,7 @@
 		<div class="well">
 			<p>VoIP ID homepage</p>
 			
-			<p>Admin and user manuals available at a< href="http://github.com/antonraharja/book-voip-id" target="_blank">VoIP ID Book Project</a></p>
+			<p>Admin and user manuals available at <a href="http://github.com/antonraharja/book-voip-id" target="_blank">VoIP ID Book Project</a></p>
 			
 			<p>Edit this page by editing <strong>app/views/home.blade.php</strong></p>
 		</div>
