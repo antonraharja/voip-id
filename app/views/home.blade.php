e73c8007dd7ad6ad74222ed8ff61d9438072c16d
Anton Raharja <antonrd@gmail.com>
2015-05-10 12:32:29 +0700

    update home.blade.php

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -12,17 +12,11 @@
 		{{ $homepage }}
 		@else
 		<div class="well">
-			<p>Terima kasih telah mengunjungi website kami.</p>
+			<p>VoIP ID homepage</p>
 			
-			<p>Saat ini kami masih dalam persiapan rilis. Pengembangan dan perbaikan masih terus dilakukan. Anda dapat mengikuti perkembangannya <a href="http://s.id/bagitugasteleponrakyat" target="_blank">disini</a>.</p>
+			<p>Admin and user manuals available at a< href="http://github.com/antonraharja/book-voip-id" target="_blank">VoIP ID Book Project</a></p>
 			
-			<p>Kendati demikian, layanan pada website ini sudah berjalan dengan cukup baik. Apabila ada kekurangan atau kesalahan biasanya tidak lama kemudian langsung diperbaiki.</p>
-			
-			<p>Oleh karena ini besar harapan kami agar anda dapat ikut berpartisipasi. Daftarkanlah diri anda sebagai Manager dan tambahkan Domain anda.</p>
-			
-			<p>Baca petunjuk untuk memulai <a href="http://s.id/memulaiteleponrakyat" target="_blank">disini</a>.</p>
-			
-			<p>Apabila ditemui kesulitan atau ingin berdiskusi dengan kami maka sila hubungi kami dengan bergabung di Facebook Group kami <a href="https://www.facebook.com/groups/voipid/" target="_blank">disini</a>.</p>
+			<p>Edit this page by editing <strong>app/views/home.blade.php</strong></p>
 		</div>
 		@endif
 	</div>	
