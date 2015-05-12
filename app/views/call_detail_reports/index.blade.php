ce424e3fa986fd602692935241254bd18591abda
Rolly Maulana Awangga <rolly@awang.ga>
2015-05-12 20:44:06 +0700

    penambahan filter time di cdr

--- app/views/call_detail_reports/index.blade.php
+++ app/views/call_detail_reports/index.blade.php
@@ -43,7 +43,7 @@
         	<div class="col-md-4">
             </div>
         </div><br>
-        <!--
+        
         <div class="row">
         	<div class="col-md-8">
         		<div class="col-md-4">
@@ -70,7 +70,7 @@
             </div>
             <div class="col-md-4"></div>
         </div><br>
-        -->
+        
         <div class="row">
         	<div class="col-md-8">
         		<div class="col-md-4">
