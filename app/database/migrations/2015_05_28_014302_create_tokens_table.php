ea9353b316519efa3834344f3bb77ea75faaa9c8
Rolly Maulana Awangga <rolly@awang.ga>
2015-05-28 01:48:05 +0700

    create tokens table for preparing API

---
+++ app/database/migrations/2015_05_28_014302_create_tokens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTokensTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tokens', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_id');
+			$table->string('token')->index();
+			$table->timestamps('created_at');
+			$table->timestamps('updated_at');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tokens');
+	}
+
+}
