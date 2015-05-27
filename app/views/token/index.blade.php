b5675b8b915993ffb1a4375fbae5008ed07605fb
Rolly Maulana Awangga <rolly@awang.ga>
2015-05-28 01:27:32 +0700

    fix view for token table

--- app/views/token/index.blade.php
+++ app/views/token/index.blade.php
@@ -13,18 +13,14 @@
 		@include('template.messages')
 
         <br>
-        {{ Form::open(array('url' => 'token/search', 'method' => 'post', 'class'=> 'form-inline')) }}
+        {{ Form::open(array('url' => 'token', 'method' => 'post', 'class'=> 'form-inline')) }}
 
         <div class="form-group">
-             @if(Auth::user()->status == 2)
-            {{ Form::select('search_category', array('Search','domain' => 'Domain', 'owner' => 'Owner', 'prefix' => 'Prefix', 'description' => 'Description'), $selected['search_category'], array('class' => 'form-control input-sm')) }}
-            @else
-            {{ Form::select('search_category', array('Search','domain' => 'Domain', 'prefix' => 'Prefix', 'description' => 'Description'), $selected['search_category'], array('class' => 'form-control input-sm')) }}
-            @endif
+             Keyword
         </div>
 
         <div class="form-group">
-            {{ Form::text('search_keyword', $selected['search_keyword'], array('class' => 'form-control input-sm')) }}
+            {{ Form::text('keyword', '', array('class' => 'form-control input-sm')) }}
         </div>
 
         {{ Form::submit('Generate', array('class' => 'btn btn-primary btn-sm')) }}
@@ -32,27 +28,21 @@
         {{ Form::close() }}
         <br>
 
-		<a href="{{ url('token/add') }}"><span class="glyphicon glyphicon-plus"></span> {{ _('Add') }}</a>
 		<!-- <div  class="table-responsive"> -->
             <table id="enable_pagination" class="table table-striped table-hover dt-responsive" cellspacing="0" width="100%">
                 <thead> <tr>
-                    @if(Auth::user()->status == 2)
-                    <th>{{ _('Owner') }}</th>
-                    @endif
+                    
                     <th>{{ _('Token') }}</th>
                     <th>{{ _('Created At') }}</th>
                     <th class="text-center">{{ _('Action') }}</th>
                 </tr></thead>
                 <tbody>
-                @foreach ($domains as $domain)
+                @foreach ($tokens as $token)
                 <tr>
-                    @if(Auth::user()->status == 2)
-                    <td>{{ $domain->user->username }}</td>
-                    @endif
-                    <td><a target="_blank" href="http://{{ $domain->domain }}">http://{{ $domain->domain }}</a></td>
-                    <td>{{ $domain->sip_server }}</td>
+                    <td>{{ $token->token }}</a></td>
+                    <td>{{ $token->created_at }}</td>
                     <td class="text-center action">
-                        <a class="tooltips" href="{{ url('domain/delete/'.$domain->id) }}" title="{{ _('Delete token') }}"><span class="glyphicon glyphicon-trash" onclick="return confirm('{{ _('Are you sure want to delete?') }}')"></span></a>
+                        <a class="tooltips" href="{{ url('token/delete/'.$token->id) }}" title="{{ _('Delete token') }}"><span class="glyphicon glyphicon-trash" onclick="return confirm('{{ _('Are you sure want to delete?') }}')"></span></a>
                     </td>
                 </tr>
                 @endforeach
