ef626bf0613e95c0c14481ab9e1cf5af65e6a98a
Rolly Maulana Awangga <rolly@awang.ga>
2015-05-28 22:23:01 +0700

    adding private function for get user name status and id

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -1,13 +1,13 @@
 <?php
 
-class OnlinePhonesController extends \BaseController {
+class ApiController extends \BaseController {
 
 	/**
 	 * Instantiate a new OnlinePhonesController instance.
 	 */
 	public function __construct() {
 
-		$this->beforeFilter('auth');
+		//$this->beforeFilter('auth');
 
 	}
 
@@ -16,26 +16,31 @@ class OnlinePhonesController extends \BaseController {
 	 *
 	 * @return Response
 	 */
-	public function getIndex()
+	public function postOnlinephonelist()
 	{
-		$status = Auth::user()->status;
-		if($status == 2){
-			$online_phone = OnlinePhone::all();
-		}elseif($status == 3){
-			$domain = Domain::whereUserId(Auth::user()->id)->get(array('sip_server'));
-			$sip_server = array();
-			foreach ($domain as $row) {
-				$sip_server[] = $row['sip_server'];
-			}
-			if($sip_server){
-				$online_phone = OnlinePhone::whereIn('sip_server', $sip_server)->get();
-			} else $online_phone = [];
-		}else{
-			$sip_server = Domain::find(Cookie::get('domain_hash'))->sip_server;
-			$online_phone = OnlinePhone::whereSipServer($sip_server)->get();
-		}
+		$token = Input::only('token');
+		$user_id = $this->_getUserId($token['token']);
+		echo $user_id;
+		echo $this->_getUsername($user_id);
+		echo $this->_getUserStatus($user_id);
+		//var_dump($varnya);
 
-		return View::make('online_phone.index')->with('online_phones', $online_phone);
+		//return View::make('online_phone.index')->with('online_phones', $online_phone);
+	}
+	
+	private function _getUserId($token){
+		$user = Token::where('token',$token)->get(['user_id']);
+		return $user[0]->user_id;
+	}
+	
+	private function _getUsername($user_id){
+		$username = User::find($user_id);
+		return $username->username;
+	}
+	
+	private function _getUserStatus($user_id){
+		$status = User::find($user_id);
+		return $status->status;
 	}
 	
 	private function _haveOnlinePhone($sip_server){
