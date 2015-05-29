c268b7b3fc12b05b2a0f1c41f992fa4dbb2b877f
Rolly Maulana Awangga <rolly@awang.ga>
2015-05-29 21:02:04 +0700

    add exception kalou token tidal ada di tabel

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -20,32 +20,36 @@ class ApiController extends \BaseController {
 	{
 		$token = Input::only('token');
 		$user_id = $this->_getUserId($token['token']);
-		$status = $this->_getUserStatus($user_id);
-		
-		if($status == 2){
-			$online_phone = OnlinePhone::all();
-		}elseif($status == 3){
-			$domain = Domain::whereUserId($user_id)->get(array('sip_server'));
-			$sip_server = array();
-			foreach ($domain as $row) {
-				$sip_server[] = $row['sip_server'];
+		$online_phone = '';
+		if($user_id){
+			$status = $this->_getUserStatus($user_id);
+			
+			if($status == 2){
+				$online_phone = OnlinePhone::all();
+			}elseif($status == 3){
+				$domain = Domain::whereUserId($user_id)->get(array('sip_server'));
+				$sip_server = array();
+				foreach ($domain as $row) {
+					$sip_server[] = $row['sip_server'];
+				}
+				if($sip_server){
+					$online_phone = OnlinePhone::whereIn('sip_server', $sip_server)->get();
+				} else $online_phone = [];
+			}else{
+				$sip_server = Domain::whereUserId($user_id)->first()->sip_server;
+				$online_phone = OnlinePhone::whereSipServer($sip_server)->get();
 			}
-			if($sip_server){
-				$online_phone = OnlinePhone::whereIn('sip_server', $sip_server)->get();
-			} else $online_phone = [];
-		}else{
-			$sip_server = Domain::whereUserId($user_id)->first()->sip_server;
-			$online_phone = OnlinePhone::whereSipServer($sip_server)->get();
 		}
-		
 		echo $online_phone;
 
 		//return View::make('online_phone.index')->with('online_phones', $online_phone);
 	}
 	
 	private function _getUserId($token){
-		$user = Token::where('token',$token)->get(['user_id']);
-		return $user[0]->user_id;
+		if(Token::where('token',$token)->exists()){
+			$user = Token::where('token',$token)->get(['user_id']);
+			return $user[0]->user_id;
+		}else return false;
 	}
 	
 	private function _getUsername($user_id){
