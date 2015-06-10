5ba293f1e7a6588a56aa9b4171709a738ebcf5f6
Rolly Maulana Awangga <rolly@awang.ga>
2015-06-10 19:49:45 +0700

    fix #19 add user list view with token and domain parameter

--- app/views/api/userlist.blade.php
+++ app/views/api/userlist.blade.php
@@ -1,3 +1,7 @@
+<?php 
+$length = count($users);
+$i =1;
+?>
 [
 @foreach ($users as $user)
            {
@@ -5,6 +9,11 @@
            "name":"{{ $user->profile->first_name }} {{ $user->profile->last_name }}",
            "email":"{{ $user->email }}"
            }
+           @if ($i < $length)
            ,
+           @endif
+           <?php
+           $i++;
+           ?>
 @endforeach
 ]
