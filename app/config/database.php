09f044c96b00058a0f6b892ba915a2bfbbc4f50f
Rolly Maulana Awangga <rolly@awang.ga>
2015-06-17 22:04:32 +0700

    reconfigure for root no password

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'voip_id',
 			'username'  => 'root',
-			'password'  => 'password',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
